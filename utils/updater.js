1d47ce4dfba7dd0e03cd1fd777a8e4a76a45ee9a
Jake Podell <podell6@gmail.com>
2017-01-08 21:32:10 -0500

    converted the updater to single semester to prevent any errors.

--- utils/updater.js
+++ utils/updater.js
@@ -2,86 +2,78 @@ var express = require('express');
 var router = express.Router();
 var mysql = require("mysql");
 var request = require('request-promise');
+var fs = require('fs');
+
+var semester = process.argv[2];
+console.log(semester);
 
 const CORNELL_API = {
-    rosters: "https://classes.cornell.edu/api/2.0/config/rosters.json",
     subjects: "https://classes.cornell.edu/api/2.0/config/subjects.json",
     classes: "https://classes.cornell.edu/api/2.0/search/classes.json",
 }
-/**
- * @return all available rosters from the api
- */
- var getRosters = function() {
-    return request(CORNELL_API.rosters)
-        .then(function(res) {
-            var response = JSON.parse(res);
-            var rosters = response.data.rosters;
-            var validRosters = rosters.map(function(roster) {
-                return(roster.slug);
-            }).filter(function(roster) {
-                // return /(SP|FA)\d+/.test(roster);
-                return /(FA14|SP15|FA15)/.test(roster);
-            });
-            return(validRosters);
-        })
-        .catch(function(err) {
-            console.log("error when obtaining the rosters");
-        })
+
+function pad(width, string, padding) {
+    return (width <= string.length) ? string : pad(width, padding + string, padding)
 }
 
-/**
- * @return the subjects for the given roster
- * @param roster the roster to to get the subjects for
- */
-var getSubjects = function(rosters) {
-     return Promise.all(rosters.map(function(roster) {
-        return request(CORNELL_API.subjects + "?roster=" + roster)
+fs.writeFile(semester + '.txt', "=============== " + semester + "===============\n____________________________________________________\n", function (err) {
+    if (err)
+        return console.log(err);
+});
+
+request(CORNELL_API.subjects + "?roster=" + semester, {json: true})
             .then(function(res) {
-                var response = JSON.parse(res);
-                var subjects = response.data.subjects;
-                return {
-                    roster: roster,
-                    subjects: subjects,
-                };
+                var subjects = res.data.subjects;
+                subjects.map(function (subject) {
+                    request(CORNELL_API.classes + "?roster=" + semester + "&" + "subject=" + subject.value, {json: true})
+                        .then(function (res) {
+                            var courses = res.data.classes;
+                            var classes = {
+                                roster: semester,
+                                subject: subject.descr,
+                                courses: courses.map(function (course, index) {
+                                    fs.appendFile(semester + '.txt'," | " +pad(8, subject.value, " ") + " | " + pad(40, course.titleShort, " ") + "|\n_______________________________________________________\n", function (err) {
+                                        if (err)
+                                            return console.log(err);
+                                    });
+                                    return course.titleLong;
+                                }),
+                            };
+                        })
+                })
             })
             .catch(function(err) {
                 console.log("error when obtaining the subjects");
             })
-
-     }))
-}
 
 /**
  * @return the classes for a given roster and subject
  * @param roster
  * @param subject
  */
-var getClasses = function(rosters) {
-    var counter = 0;
-    return rosters.map(function(roster) {
-        return Promise.all(roster.subjects.map(function(subject, index) {
-            return request(CORNELL_API.classes + "?roster=" + roster.roster + "&" + "subject=" + subject.value)
-                .then(function(res) {
-                    var response = JSON.parse(res);
-                    var courses = response.data.classes;
-                    console.log(counter++);
-                    return {
-                        roster: roster.roster,
-                        subject: subject.descr,
-                        courses: courses.map(function (course, index) {
-                            return course.titleLong;
-                        }),
-                    }
-                })
-                .catch(function(err) {
-                    console.log("error when obtaining the courses");
-                })
-        })).then(function(data) {
-            console.log(data);
-        })
-    })
-}
+// var getClasses = function(rosters) {
+//     console.log(rosters)
+//     // return rosters.map(function(roster) {
+//     //     return Promise.all(roster.subjects.map(function(subject) {
+//     //         return request(CORNELL_API.classes + "?roster=" + roster.roster + "&" + "subject=" + subject.value)
+//     //             .then(function(res) {
+//     //                 var courses = res.data.classes;
+//     //                 return {
+//     //                     roster: roster.roster,
+//     //                     subject: subject.descr,
+//     //                     courses: courses.map(function (course, index) {
+//     //                         return course.titleLong;
+//     //                     }),
+//     //                 }
+//     //             })
+//     //             .catch(function(err) {
+//     //                 console.log(err);
+//     //             })
+//     //     })).then(function(data) {
+//     //         console.log(data);
+//     //     })
+//     // })
+// }
 
-getRosters()
-    .then(getSubjects)
-    .then(getClasses);
+// getSubjects(semester)
+    // .then(getClasses);
